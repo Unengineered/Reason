6c05eb0869d496c964fef23141912db7dfd3e282
akshita251 <akshitaupadhyay.02@gmail.com>
2021-07-30 17:36:09 +0530

    FIXED: store_id param

--- Product/routes/product.route.js
+++ Product/routes/product.route.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const productController = require('../controllers/product.controller')
 
-router.get('/:product_id', productController.product)
 router.post('/addproducts', productController.addProducts)
 router.get('/allProductsMongo', productController.allProductsMongo)
 router.get('/allProductsSQL', productController.allProductsSQL)
 router.post('/deleteProducts', productController.deleteProducts)
 router.post('/updateProducts', productController.updateProducts)
+router.get('/:product_id', productController.product)
+
 
 module.exports = router
--- Store/routes/store.route.js
+++ Store/routes/store.route.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const storeController = require('../controllers/store.controller')
 
-router.get('/:store_id', storeController.store)
 router.get('/allStoresSQL', storeController.allStoresSQL)
 router.get('/allStoresMongo', storeController.allStoresMongo)
 router.post('/addStores', storeController.addStores)
 router.post('/deleteStores', storeController.deleteStores)
 router.post('/updateStores', storeController.updateStores)
+router.get('/:store_id', storeController.store)
+
 
 module.exports = router
